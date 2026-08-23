ee7fa33d76e7701dcb4ff966fd5f4c0dff341dd9
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-23 06:59:00 +0300

    Enable hidden API descriptor remapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.remap) apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
 }
 
