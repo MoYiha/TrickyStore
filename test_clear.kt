332f750f64952e1b5fd07880850ebdaef3396d52
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-24 06:23:17 +0300

    Delete test_clear.kt

--- test_clear.kt
+++
@@ -1,9 +0,0 @@
-import cleveres.tricky.cleverestech.util.KeyboxVerifier
-import java.io.File
-fun main() {
-    println("Start")
-    val f = File("/data/adb/test")
-    KeyboxVerifier.setCacheRootForTesting(f)
-    KeyboxVerifier.clearMemoryCacheForTesting()
-    println("End")
-}
