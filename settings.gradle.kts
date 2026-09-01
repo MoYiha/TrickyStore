c8b666014c6de668f97096e9d3f645d70f49e58a
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-01 06:22:06 +0300

    🧪 [build] fix: resolve bouncycastle resolution and codeql setup (#1159)
    * 🧪 [build] fix: resolve bouncycastle resolution and codeql setup
    
    * 🧪 [build] fix: resolve bouncycastle resolution and codeql setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://downloads.bouncycastle.org/java/maven")
+            content {
+                includeGroup("org.bouncycastle")
+            }
+        }
     }
 }
 
